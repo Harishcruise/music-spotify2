b1fed3220876de20f99541cb735c4eec42d55c1e
Harishcruise <harishbarath2001@gmail.com>
2022-03-13 21:48:01 +0530

    initial

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HomeIcon,SearchIcon,LibraryIcon,PlusCircleIcon} from '@heroicons/react/outline'
+function Sidebar() {
+  return (
+    <div>
+        <div>
+            <HomeIcon className='h-5 w-5' />
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
