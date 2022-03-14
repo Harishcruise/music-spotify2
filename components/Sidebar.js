f9e1821340c7f340db4090a5a64babf59e45c881
Harishcruise <harishbarath2001@gmail.com>
2022-03-14 21:43:41 +0530

    sidebar phase-1 created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import {HomeIcon,SearchIcon,LibraryIcon,PlusCircleIcon} from '@heroicons/react/outline'
+import {HomeIcon,SearchIcon,LibraryIcon,PlusCircleIcon, HeartIcon, RssIcon} from '@heroicons/react/outline'
 function Sidebar() {
   return (
-    <div>
+    <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
         <div>
+            <button className='flex items-center space-x-2 hover:text-white'>
             <HomeIcon className='h-5 w-5' />
+            <p>Home</p>
+            </button>
+            <button className='flex items-center space-x-2 hover:text-white '>
+            <SearchIcon className='h-5 w-5' />
+            <p>Search</p>
+            </button>
+            <button className='flex items-center space-x-2 hover:text-white pb-2'>
+            <LibraryIcon className='h-5 w-5' />
+            <p>Your Library</p>
+            </button>
+            <hr className='border-t-[0.1px] border-gray-900' />
+            <button className='flex items-center space-x-2 hover:text-white pt-2'>
+            <PlusCircleIcon className='h-5 w-5' />
+            <p>Create Playlist</p>
+            </button>
+            <button className='flex items-center space-x-2 hover:text-white'>
+            <HeartIcon className='h-5 w-5' />
+            <p>Liked Songs</p>
+            </button>
+            <button className='flex items-center space-x-2 hover:text-white pb-2'>
+            <RssIcon className='h-5 w-5' />
+            <p>Your Episodes</p>
+            </button>
+            <hr className='border-t-[0.1px] border-gray-900' />
         </div>
     </div>
   )
